884001cb7b1bd077a0d9444c616c0ded9b9d5c7c
915615783 <915615783@qq.com>
2020-04-28 14:45:35 +0800

    slr1

--- grammatical_analysis/SLR1.py
+++ grammatical_analysis/SLR1.py
@@ -15,6 +15,10 @@ class Item():
         self.BNF_id = BNF_id
         self.dot = dot
 
+    def next_item(self):
+        '''返回一个新的item对象， dot向后移一格'''
+        return Item.from_BNF(gramma.BNF.id2BNF[self.BNF_id], self.dot_index()+1)
+
     def dot_index(self):
         return self.right.index(self.dot)
 
@@ -55,7 +59,23 @@ class Item():
 class SLR1():
     def __init__(self, augment_gramma):
         self.G = augment_gramma
-        self.C = None   # LR(0)项集族C
+        self.C = self.item_sets()   # LR(0)项集族C list  分析时从第0个item开始
+
+    def item_sets(self):
+        '''构建LR(0)项集族C'''
+        C = []
+        queue = [self.closure({Item.from_BNF(self.G.BNFs[0], 0)})]
+        while len(queue) != 0:
+            item_set = queue.pop(0)
+            if item_set in C:
+                continue
+            for v in ((self.G.Vt | self.G.Vn) - {'epsilon'}):
+                goto_set = self.goto(item_set, v)
+                if len(goto_set) == 0:
+                    continue
+                queue.append(goto_set)
+            C.append(item_set)
+        return C
 
     def closure(self, item_set):
         result = set([])
@@ -71,7 +91,68 @@ class SLR1():
                         queue.append(Item.from_BNF(bnf, 0))
             result.add(item)
         return result
+
+    def goto(self, item_set, x):
+        '''
+        x: str. not include epsilon
+        '''
+        result = set([])
+        for item in item_set:
+            if item.is_reducible() == False:
+                if x == item.right[item.dot_index() + 1]:
+                    result.add(item.next_item())
+        return self.closure(result)
+
+    def get_action_goto(self):
+        '''
+        获取slr1分析表
+        return action(dict), goto(dict)
+        action: {(int, str): str}
+        goto: {(int, str): int}
+        '''
+        action = {}
+        goto = {}
+        for i, item_set in enumerate(self.C):
+            for item in item_set:
+                if item.is_reducible() == False:
+                    # 移进项目
+                    x = item.right[item.dot_index()+1]
+                    if x in self.G.Vt:
+                        if action.get((i, x), None) == None:
+                            action[(i, x)] = 's' + str(self.C.index(self.goto(item_set, x)))
+                        else:
+                            if action[(i, x)] != ('s' + str(self.C.index(self.goto(item_set, x)))):
+                                raise Exception('不是slr1语法,无法建立分析表')
+                    elif x in self.G.Vn:
+                        if goto.get((i, x), None) == None:
+                            goto[(i, x)] = self.C.index(self.goto(item_set, x))
+                        else:
+                            if goto[(i, x)] != self.C.index(self.goto(item_set, x)):
+                                raise Exception('不是slr1语法,无法建立分析表')
+                else:
+                    # 归约项目
+                    A = item.left
+                    if A == self.G.S:
+                        action[(i, '$')] = 'acc'
+                    else:
+                        target = 'r' + str(item.BNF_id)
+                        for x in self.G.FOLLOW(A):
+                            if action.get((i, x), None) == None:
+                                action[(i, x)] = target
+                            else:
+                                if action[(i, x)] != target:
+                                    print(item, x)
+                                    raise Exception('不是slr1语法,无法建立分析表')
+        return action, goto
+                    
+
                         
+
+
+
+    
+                        
+    
 
 
 
--- main.py
+++ main.py
@@ -4,22 +4,12 @@ from grammatical_analysis import gramma, SLR1
 G = gramma.Gramma('grammatical_analysis/gramma.txt', lexical_analysis.vocab_table)
 
 # print(gramma.BNF.id_count)
-a = SLR1.Item.from_BNF(G.BNFs[2], 0)
-print(a)
-b = SLR1.Item.from_BNF(G.BNFs[1], 0)
-print(b)
+
 slr1 =SLR1.SLR1(G)
-close = slr1.closure({b})
-print(slr1.closure({a, b}) == close)
-print()
-for i in close:
-    print(i)
+
+acction, goto = slr1.get_action_goto()
+print(acction, goto)
 
 
-# aa = {a}
-# bb = {b}
-# print(aa == bb)
-# for i in range(len(G.BNFs)):
-#     print(i, G.BNFs[i])
 
 
