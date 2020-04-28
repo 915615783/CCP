a5e468ee661f53e59e524b6702d771aa0abcd7ce
915615783 <915615783@qq.com>
2020-04-28 12:57:50 +0800

    closure of item set

--- grammatical_analysis/SLR1.py
+++ grammatical_analysis/SLR1.py
@@ -11,9 +11,19 @@ class Item():
         self.id = Item.id_count
         Item.id_count += 1
         self.left = left
-        self.right = tuple(right)
+        self.right = tuple([i for i in right if i != 'epsilon'])
         self.BNF_id = BNF_id
         self.dot = dot
+
+    def dot_index(self):
+        return self.right.index(self.dot)
+
+    def is_reducible(self):
+        '''是否可归约'''
+        return self.right.index(self.dot) == len(self.right)-1
+
+    def __hash__(self):
+        return hash(self.__str__())
 
     @classmethod
     def from_BNF(cls, bnf, dot_pos, dot='dot'):
@@ -42,3 +52,27 @@ class Item():
     def __eq__(self, other):
         return (self.left == other.left) and (self.right == other.right)
     
+class SLR1():
+    def __init__(self, augment_gramma):
+        self.G = augment_gramma
+        self.C = None   # LR(0)项集族C
+
+    def closure(self, item_set):
+        result = set([])
+        queue = list(item_set)
+        while len(queue) != 0:
+            item = queue.pop(0)
+            if item in result:
+                continue
+            if item.is_reducible() == False:
+                B = item.right[item.dot_index() + 1]
+                for bnf in self.G.BNFs:
+                    if B == bnf.left:
+                        queue.append(Item.from_BNF(bnf, 0))
+            result.add(item)
+        return result
+                        
+
+
+
+
--- main.py
+++ main.py
@@ -3,7 +3,23 @@ from grammatical_analysis import gramma, SLR1
 
 G = gramma.Gramma('grammatical_analysis/gramma.txt', lexical_analysis.vocab_table)
 
-print(gramma.BNF.id_count)
-a = SLR1.Item.from_BNF(G.BNFs[13], 4)
+# print(gramma.BNF.id_count)
+a = SLR1.Item.from_BNF(G.BNFs[2], 0)
 print(a)
+b = SLR1.Item.from_BNF(G.BNFs[1], 0)
+print(b)
+slr1 =SLR1.SLR1(G)
+close = slr1.closure({b})
+print(slr1.closure({a, b}) == close)
+print()
+for i in close:
+    print(i)
+
+
+# aa = {a}
+# bb = {b}
+# print(aa == bb)
+# for i in range(len(G.BNFs)):
+#     print(i, G.BNFs[i])
+
 
